19ff33c478a265863f570dd5ae7b91ef7d952241
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-07-20 13:03:15 +0300

    Changes classes for hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { apiSearch } from './services/API';
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -18,29 +18,20 @@ export function App() {
   const [alt, setAlt] = useState('');
   const [error, setError] = useState(null);
 
-  // useEffect(() => { 
-  //   if (searchValue === "") {
-  //     return;
-  //   }
+  useEffect(() => { 
+    
+    if (searchValue === "") {
+      return;
+    }
+    
+    setIsLoading(true);
+    apiResponse();
 
-  //   setPictures([]);
-  //   setIsLoading(true);
-  //   apiResponse(page);
-       
-  // }, [searchValue]);
+  
+  }, [searchValue, page]);
+  
 
-  // useEffect(() => { 
-  //   if (page === 1) {
-  //     return;
-  //   }
-
-  //   setIsLoading(true);
-  //   apiResponse(page);
-
-  // }, [page]);
-
-
-  const apiResponse = async (page) => {
+  async function apiResponse() {
     try {
       if (searchValue !== "") {
         
@@ -73,7 +64,6 @@ export function App() {
     }     
   }
 
-
   const toggleModal = (img, title) => {
     setShowModal(!showModal);
     setLargeImg(img);
@@ -81,6 +71,7 @@ export function App() {
   }
 
   const getItems = (searchValue) => {
+    setPictures([]);
     setPage(1);
     setSearchValue(searchValue);
   }
@@ -112,118 +103,4 @@ export function App() {
     )
   }
 
-
-
-
-// export class App extends Component {
-
-//   state = {
-//     searchValue: '',
-//     pictures: [],
-//     isLoading: false,
-//     page: 1,
-//     showModal: false,
-//     loadMore: false,
-//     largeImg: '',
-//     alt: '',
-//     error: null,
-//   }
-  
-  // componentDidUpdate(_, prevState) {
-  //   if (prevState.searchValue !== this.state.searchValue) {
-  //     this.setState({
-  //       isLoading: true,
-  //       pictures: [],
-  //     });
-  //     this.apiResponse(this.state.page);
-  //   }
-    
-  //   if (this.state.searchValue === prevState.searchValue && prevState.page !== this.state.page) {
-  //     this.setState({
-  //       isLoading: true,
-  //     });
-  //     this.apiResponse(this.state.page);
-  //   }
-  // }
-
-//   apiResponse = async (page) => {
-//     try {
-//       const pictures = await apiSearch(this.state.searchValue, page);
-
-//       const msgForWrongSearch = `There is no match for "${this.state.searchValue}".`
-      
-//       if (pictures.totalHits === 0) {
-//         this.setState({ error: msgForWrongSearch });
-//       } else {
-//         this.setState({ error: null });
-//       }
-        
-//       if ((pictures.totalHits - this.state.pictures.length) > pictures.hits.length) {
-//         this.setState({ loadMore: true })
-//       } else {
-//         this.setState({ loadMore: false })
-//       }
-        
-//       this.setState(prevState => ({
-//         pictures: [...prevState.pictures, ...pictures.hits],
-//       }))    
-      
-//     } catch (error) {
-//       const msgForApiBadRespond = "Something went wrong."
-//       this.setState({ error: msgForApiBadRespond });
-//       console.log(error);   
-
-//     } finally {
-//       this.setState({isLoading: false });
-//     }     
-//   }
-
-//   toggleModal = (img, title) => {
-//       this.setState({      
-//         showModal: !this.state.showModal,
-//         largeImg: img,
-//         alt: title,
-//       })
-//   }
-  
-//   getItems = (searchValue) => {
-//     this.setState({
-//       searchValue,
-//       page: 1,
-//     });
-//   }
-
-//   handleLoadMore = () => {
-//     this.setState( prevState => ({
-//       page: prevState.page + 1,
-//     }))
-//   }
-
-//   render() {
-//     const { isLoading, error, loadMore, showModal, alt, largeImg, pictures } = this.state;
-
-//     return (
-//       <div
-//         style={{
-//           display: 'grid',
-//           gridTemplateColumns: '1fr',
-//           gridGap: 16,
-//           paddingBottom: 24,
-//         }}
-//       >
-//         <Searchbar onSubmit={this.getItems} />
-//         {error && <ErrorMsg msg={error} />}
-//         <ImageGallery items={pictures} onClick={this.toggleModal} />
-//         {isLoading &&
-//           < Loader />}
-//         {loadMore &&
-//           <Button onClick={this.handleLoadMore} />}  
-//         {showModal &&
-//           <Modal onClose={this.toggleModal} largeImg={largeImg} alt={alt}/>        
-//         }
-//       </div>
-//     )
-//   }
-
-// }
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,66 @@
-import React, {Component} from "react"
+import {useEffect} from "react"
 import { Overlay, ModalEl } from "./Modal.styled"
 
-export class Modal extends Component {
+export const Modal = ({onClose, largeImg, alt}) => {
 
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    }
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
 
-    componentWillUnmount () {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [])
+    
 
-    handleKeyDown = e => {
+    const handleKeyDown = e => {
         if (e.code === "Escape") {
-            this.props.onClose(this.props.largeImg);
+            onClose(largeImg);
         }
     }
 
-    handleBackdropClick = e => {
+    const handleBackdropClick = e => {
         if (e.currentTarget === e.target) {
-            this.props.onClose(this.props.largeImg);            
+            onClose(largeImg);            
         }
     }
 
-    render() {
-    return <Overlay onClick ={this.handleBackdropClick}>
-                <ModalEl>
-                    <img src={this.props.largeImg} alt={this.props.alt} />
-                </ModalEl>
-            </Overlay>
-    }
-
+    return <Overlay onClick ={handleBackdropClick}>
+        <ModalEl>
+            <img src={largeImg} alt={alt} />
+        </ModalEl>
+    </Overlay>
+    
 }
+
+
+// export class Modal extends Component {
+
+//     componentDidMount() {
+//         window.addEventListener('keydown', this.handleKeyDown);
+//     }
+
+//     componentWillUnmount () {
+//         window.removeEventListener('keydown', this.handleKeyDown);
+//     }
+
+//     handleKeyDown = e => {
+//         if (e.code === "Escape") {
+//             this.props.onClose(this.props.largeImg);
+//         }
+//     }
+
+//     handleBackdropClick = e => {
+//         if (e.currentTarget === e.target) {
+//             this.props.onClose(this.props.largeImg);            
+//         }
+//     }
+
+//     render() {
+//     return <Overlay onClick ={this.handleBackdropClick}>
+//                 <ModalEl>
+//                     <img src={this.props.largeImg} alt={this.props.alt} />
+//                 </ModalEl>
+//             </Overlay>
+//     }
+
+// }
